54afef48ee4c9a6b31369c4351d5753b02441432
Arab45 <horlakunleolayiwola@gmail.com>
2025-04-25 05:42:32 -0700

    add splash and exchange images; update layout and create start screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,63 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Image, Pressable, SafeAreaView, StyleSheet, Text, Touchable, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View></View>
+      <View style={styles.exchangeContent}>
+        <Image source={require("../assets/folder/money-exchange.png")} />
+        <Text style={[styles.contentText, {fontSize: 18, fontWeight: "bold"}]}>Don't miss your earnings</Text>
+        <View style={styles.relativeContainer}>
+          <Text style={styles.contentText}>Stay updated updated on your earning</Text>
+          <Text style={styles.contentText}>allowing us to send you important</Text>
+          <Text style={styles.contentText}>notifcation</Text>
+        </View>
+        <Text style={styles.enableNote}>Enable notification</Text>
+      </View>
+    <Pressable style={styles.nextButton} onPress={() => {router.push("/start")}}>
+      <Text style={styles.nextText}>Next</Text>
+    </Pressable>
+    </SafeAreaView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  exchangeContent:{
+    gap: 30,
+    alignItems: "center"
+  },
+  contentText:{
+    color: "#000056",
+    fontSize: 16
+  },
+  relativeContainer: {
+    alignItems: "center"
+  },
+  enableNote: {
+    color: "#000056",
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  nextButton: {
+    backgroundColor: "#000056",
+    width: "50%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 24,
+    marginBottom: 20
+  },
+  nextText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold"
+  }
+})
---
+++ app/start.tsx
@@ -0,0 +1,76 @@
+import { Image, Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import  {MaterialIcons}  from "@expo/vector-icons";
+import { router } from "expo-router";
+
+export default function Start() {
+   return (
+      <SafeAreaView style={styles.container}>
+         <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: 30}}>
+         <View style={styles.backIcon}>
+            <MaterialIcons name="arrow-back" size={24} color="#fff" onPress={() => { router.push("/")}}/>
+         </View>
+         </View>
+         <View style={styles.exchangeContent}>
+            <Image source={require("../assets/folder/exchange-image.png")} />
+            <Text style={styles.secureText}>Easy and Secure</Text>
+            <View style={styles.relativeContainer}>
+               <Text style={styles.contentText}>Bit enables user to mine crypto currency</Text>
+               <Text style={styles.contentText}>effeciently provide real-time stats, secure</Text>
+               <Text style={styles.contentText}>wallet integration and easy payout</Text>
+            </View>
+         </View>
+         <View style={{flex: 1, justifyContent: "flex-end", alignItems: "center", paddingBottom: 24}}>
+         <Pressable style={styles.getButton} onPress={() => { router.push("/") }}>
+            <Text style={styles.getText}>Get Started</Text>
+         </Pressable>
+         </View>
+      </SafeAreaView>
+   );
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      justifyContent: "space-between",
+      // alignItems: "center",
+   },
+   exchangeContent: {
+      gap: 30,
+      alignItems: "center"
+   },
+   contentText: {
+      color: "#000056",
+      fontSize: 16
+   },
+   relativeContainer: {
+      alignItems: "center"
+   },
+   secureText: {
+      color: "#000056",
+      fontSize: 30,
+      fontWeight: "bold"
+   },
+   getButton: {
+      backgroundColor: "#000056",
+      width: "50%",
+      height: 50,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 24,
+      marginBottom: 24
+   },
+   getText: {
+      color: "#fff",
+      fontSize: 18,
+      fontWeight: "bold"
+   },
+   backIcon: {
+      backgroundColor: "#000056",
+    marginTop: 24,
+      width: 24,
+      alignItems: "center",
+      borderRadius: 64,
+      borderColor: "black",
+   }
+})
