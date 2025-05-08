959d291b569b0c93c9a4f1b0519d055fd8d33633
Arab45 <horlakunleolayiwola@gmail.com>
2025-05-08 07:01:01 -0700

    enhance transaction screen; increase font size for better readability of transaction history

--- app/(auth)/(tabs)/transaction.tsx
+++ app/(auth)/(tabs)/transaction.tsx
@@ -5,7 +5,7 @@ export default function Transaction() {
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <Text>Transactions History</Text>
+                <Text style={{fontSize: 16}}>Transactions History</Text>
                 <View style={{ backgroundColor: "#000056", flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%", height: 100}}>
                     <Image source={require("../../../assets/folder/transaction.png")} />
                     <View>
@@ -13,7 +13,7 @@ export default function Transaction() {
                   <Text style={{fontSize: 16, fontWeight: "bold"}}>4904,0486 bit</Text>  
                     </View>
                 </View>
-                <Text>History</Text>
+                <Text style={{fontSize: 16}}>History</Text>
             </View>
         </SafeAreaView>
     )
