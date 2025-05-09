ae063bbbe9ac25afb4f862b0450bc6ab3d863279
Arab45 <horlakunleolayiwola@gmail.com>
2025-05-09 13:46:56 -0700

    enhance transaction screen; increase font size for total balance text

--- app/(auth)/(tabs)/transaction.tsx
+++ app/(auth)/(tabs)/transaction.tsx
@@ -9,7 +9,7 @@ export default function Transaction() {
                 <View style={{ backgroundColor: "#000056", flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%", height: 100}}>
                     <Image source={require("../../../assets/folder/transaction.png")} />
                     <View>
-                  <Text>Total Balance</Text>
+                  <Text style={{fontSize: 16}}>Total Balance</Text>
                   <Text style={{fontSize: 16, fontWeight: "bold"}}>4904,0486 bit</Text>  
                     </View>
                 </View>
